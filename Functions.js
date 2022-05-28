8a78a2d1b2271e1f104fdb92b3e74325da657506
IanMRoberts <imroberts30@gmail.com>
2022-05-27 23:58:25 -0400

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -111,26 +111,28 @@ function GetImages(Artists,Size) {
           Artist.listeners = data.artist.stats.listeners;
           Artist.onTour = data.artist.ontour;
           
-          mbid = Artist.mbid;
-          url = 'https://musicbrainz.org/ws/2/artist/' + mbid + '?inc=url-rels&fmt=json';
-          fetch(url)
-                    .then(res => res.json())
-                    .then((out) => {
-                        relations = out.relations;
-                        console.table(relations);
-                        // Find image relation
-                        for (let i = 0; i < relations.length; i++) {
-                            if (relations[i].type === 'image') {
-                                let image_url = relations[i].url.resource;
-                                if (image_url.startsWith('https://commons.wikimedia.org/wiki/File:')) {
-                                    const filename = image_url.substring(image_url.lastIndexOf('/') + 1);
-                                    Artist.ImageUrl = 'https://commons.wikimedia.org/wiki/Special:Redirect/file/' + filename;
-                                }
-                                console.log(image_url);
-                                success(image_url);
-                            }
-                        }
-                    }
+          const mbid = Artist.mbid;
+          const url = 'https://musicbrainz.org/ws/2/artist/' + mbid + '?inc=url-rels&fmt=json';
+          console.log(url);
+           fetch(url)
+               .then(res => res.json())
+               .then((out) => {
+                   const relations = out.relations;
+                   console.table(relations);
+                   // Find image relation
+                   for (let i = 0; i < relations.length; i++) {
+                       if (relations[i].type === 'image') {
+                           let image_url = relations[i].url.resource;
+                           if (image_url.startsWith('https://commons.wikimedia.org/wiki/File:')) {
+                               const filename = image_url.substring(image_url.lastIndexOf('/') + 1);
+                               Artist.ImageUrl = 'https://commons.wikimedia.org/wiki/Special:Redirect/file/' + filename;
+                           }
+                           console.log(image_url);
+                           success(image_url);
+                       }
+                   }
+               })
+               .catch(err => { throw console.log(err) });
           
           
           //Artist.ImageUrl = 'https://musicbrainz.org/ws/2/artist/' + Artist.mbid + '?inc=url-rels&fmt=json';
